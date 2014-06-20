684f940cd06929ffafe8b10c473965f8f58bdc1a
Flozi95 <florian.olching@gmail.com>
2014-06-20 12:05:43 +0200

    License headers

--- Website/Website/Areas/Hydranten/Models/hydrant_lage.cs
+++ Website/Website/Areas/Hydranten/Models/hydrant_lage.cs
@@ -1,10 +1,23 @@
+// This file is part of AlarmWorkflow.
+// 
+// AlarmWorkflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AlarmWorkflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AlarmWorkflow.  If not, see <http://www.gnu.org/licenses/>.
+
 namespace AlarmWorkflow.Website.Reports.Areas.Hydranten.Models
 {
-    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("hydranten.hydrant_lage")]
     public partial class Hydrant_Lage
